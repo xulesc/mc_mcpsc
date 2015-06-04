6164bac939271e302b890c802015403deb374e4a
xule.sc <anuj.sharma80@gmail.com>
2015-06-04 15:34:50 +0300

    prep

--- scripts/make_upper_triangular.py
+++ scripts/make_upper_triangular.py
@@ -6,7 +6,7 @@ def process(dirname, flistFile, jlistFile):
   files = os.listdir(dirname)
   pairs = []
   for x in range(0, len(files)):
-    for y  in range(x, len(files)):
+    for y  in range(x + 1, len(files)):
       pairs.append('%d %d' %(x, y))
   flistFile.write('%d\n' %len(files))      
   flistFile.write('%s' %'\n'.join(files))
